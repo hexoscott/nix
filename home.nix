c8d42503e0c3fd153728d16747a1bb794912e03f
Scott Fairclough <scott@hexosoft.co.uk>
2025-04-11 08:40:58 +0100

    initial setup

---
+++ home.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+  # This is required information for home-manager to do its job
+  home = {
+    stateVersion = "23.11";
+    username = "scott";
+    homeDirectory = "/Users/scott";
+    packages = [
+      pkgs.git
+      pkgs.neovim
+    ];
+  };
+
+  # This is to ensure programs are using ~/.config rather than
+  # /Users/<username/Library/whatever
+  xdg.enable = true;
+
+  programs.home-manager.enable = true;
+  # I use fish, but bash and zsh work just as well here. This will setup
+  # the shell to use home-manager properly on startup, neat!
+ # programs.zsh.enable = true;
+}
